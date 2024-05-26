8144ec2151de5fc8290ba8e95e650203fe47ec81
Bowen <BowenLiu0001@gmail.com>
2024-05-27 03:48:13 +0800

    0.0.1

---
+++ README.md
@@ -0,0 +1,21 @@
+# copy-code-with-info README
+
+This extension is used to copy code along with necessary information. The information includes the opened folder, current file, language, line numbers, and other details.
+
+One usage example is: when pasting it in Obsidian-Excalidraw, it will generate a code snippet with a backlink. Clicking on the backlink will open the corresponding folder and file in vscode, and navigate to the line number of that code snippet.
+
+## Features
+
+Copy code along with necessary information.
+
+## Requirements
+
+
+## Extension Settings
+
+
+## Known Issues
+
+
+## Release Notes
+
---
+++ src/extension.ts
@@ -0,0 +1,45 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	let cmd = vscode.commands.registerCommand('copy-code-with-info.copyCodeWithinfo', copyCodeWithinfo);
+	context.subscriptions.push(cmd);
+}
+
+function copyCodeWithinfo() {
+	vscode.window.showInformationMessage('copy code with info!');
+	const editor = vscode.window.activeTextEditor;
+	if (editor) {
+		const filePath = editor.document.fileName;
+		const fileFormat = editor.document.languageId;
+		const workspaceFolders = vscode.workspace.workspaceFolders;
+		const folderPath = (workspaceFolders && workspaceFolders?.length > 0 && workspaceFolders[0].uri.fsPath) ?? '';
+		if (!editor.selection) {
+			vscode.window.showInformationMessage('Please select code first.');
+			return;
+		}
+		const selectedText = editor.document.getText(editor.selection);
+		const lineNum = editor.selection.start.line + 1;
+
+		const clipboardContent = JSON.stringify({
+			folderPath,
+			filePath,
+			fileFormat,
+			lineNum,
+			text: selectedText
+		});
+
+		vscode.env.clipboard.writeText('ymjr:code-link' + clipboardContent)
+		.then(() => {
+			vscode.window.showInformationMessage('File info copied to clipboard.');
+		})
+	} else {
+		vscode.window.showWarningMessage('No active editor found.');
+	}
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
